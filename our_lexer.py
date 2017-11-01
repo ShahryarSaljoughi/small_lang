44cbd82036c9d0fccdf5986c825f50cbf6e018be
shahryar <s.shahryar75@gmail.com>
2017-11-02 02:41:39 +0330

    lexer dedicates a class name to each reserved word

--- our_lexer.py
+++ our_lexer.py
@@ -2,21 +2,49 @@ import ply.lex as lexer
 import re
 
 tokens = [
-    'const',
+    # KEYWORDS:
+    'const',  
     'var',
+    'begin',
+    'end',
     'integer',
-    'identifier',
+    'procedure',
+    'in',
+    'out',
+    'in_out',
+    'for',
+    'print',
+    'read',
+    'if',
+    'then',
+    'else',
+    'to',
+    'do',
+    'return',
+    'call',
+    'not',
+    'or',
+    'and', 
+    # IDENTIFIER
+    'identifier', 
+    # WHITESPACE
     'whitespace',
+    # SPECIAL CHARACTERS:
+    'assign_op',            # :=          
+    'semicolon',            # ;
+    'comma',                # ,
+    'l_parenthese',         # (
+    'r_parenthese',         # )
+    'minus_sign',           # -
+    'plus_sign',            # + 
+    'mul_sign',             # *
+    'divide_sign',          # /
+    'equal_sign'            # =
+    'less_equal_sing'       # <=
+    'greater_equal_sign',   # >=
+    'greater_sign',         # >
+    'less_sign',            # <
+    'not_equal_sign',       # <>
 ]
-
-t_keyword = r"""[c|C][o|O][n|N][s|S][t|T]|var|begin|end|integer|procedure|in
-                |out|in out| print|
-            """
-t_integer = r''   
-t_identifier = r''
-t_whitespace = r''
-
-
-int a = 345
 
 lex.lex()
