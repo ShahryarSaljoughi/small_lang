7f02ce6aa4d246831a4359da226fd5fb3804b599
shahryar <s.shahryar75@gmail.com>
2018-01-27 20:38:19 +0330

    our_lexer updated literals now contain > and < IN_OUT token had an issue which got fixed

--- our_lexer.py
+++ our_lexer.py
@@ -6,7 +6,7 @@ import re
 # Their class(type) is the same as their lexeme.
 # They are the last thing to be checked. 
 # So if a rule starts with a literal, it will take precedence .
-literals = ['+', '-', '/', '*', '=', ',', '(', ')', ';', '[', ']', ':']
+literals = ['+', '-', '/', '*', '=', ',', '(', ')', ';', '[', ']', ':', '<', '>']
 
 
 keywords = {                      # keys are lexeme names
@@ -15,10 +15,11 @@ keywords = {                      # keys are lexeme names
     'begin': 'BEGIN' ,
     'end': 'END',
     'integer': 'INTEGER',
+    'string': 'STRING',
     'procedure': 'PROCEDURE',
     'in': 'IN',
     'out': 'OUT',
-    'in out': 'IN_OUT',
+    # 'in out': 'IN_OUT',
     'for': 'FOR',
     'print': 'PRINT',
     'read': 'READ',
@@ -36,6 +37,7 @@ keywords = {                      # keys are lexeme names
 
 
 tokens = [
+    'IN_OUT',
     'comment',
     'identifier',
     'integer_constant',
@@ -43,25 +45,39 @@ tokens = [
     'assign_op',            # :=          
     'less_equal_sign',      # <=
     'greater_equal_sign',   # >=
-    'greater_sign',         # >
-    'less_sign',            # <
     'not_equal_sign',       # <>
 ] + list(keywords.values())
 
 t_ignore_comment = r'/\*.*\*/'
 
-t_integer_constant = r'[1-9][0-9]*'
+# t_integer_constant = r'[1-9][0-9]*'
 
 t_string_constant = r'".*"'
 
 t_assign_op = r':='
 
-t_less_equal_sign = r'>='
+t_less_equal_sign = r'<='
+
+t_greater_equal_sign = r'>='
+
+t_not_equal_sign = r'<>'
+
+def t_IN_OUT(t):
+    r'in out'
+    t.type = 'IN_OUT'
+    return t
+
 
 def t_identifier(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = keywords.get(t.value,'identifier')    # Check for reserved words
     return t
+
+def t_integer_constant(t):
+    r'[1-9][0-9]*'
+    t.value = int(t.value)
+    t.type = 'integer_constant'
+    return t  
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -81,12 +97,32 @@ def t_error(t):
 my_lexer = lexer.lex(reflags=re.IGNORECASE)
 
 data = '''
-32 + 4 * 10 + -20 *2  forget in out
-/* this is a comment */
+
+var bb: integer
+const a=10
+const b=3
+const c=5
+var result: integer
+var e: string
+
+procedure max(max: in out integer, a in integer)
+begin
+    var max: integer
+    max := a
+    if b>max then max := b 
+    if c>max then max := c
+    return;
+end
+
+begin 
+    
+    call max(a, b, c, result)
+    print("max is :", result)
+end
 '''
-my_lexer.input(data)
-while True:
-    tok = my_lexer.token()
-    if not tok: 
-        break      # No more input
-    print(tok)
+# my_lexer.input(data)
+# while True:
+#     tok = my_lexer.token()
+#     if not tok: 
+#         break      # No more input
+#     print(tok)
